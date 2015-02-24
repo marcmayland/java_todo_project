e5b080b38781a90bd2d23f0a6b619366f0636e01
grafitter <3stylemylife@gmail.com>
2015-02-24 10:06:39 +0100

    Basic GUI

--- src/java_todo_project/Java_todo_project.java
+++ src/java_todo_project/Java_todo_project.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package java_todo_project;
 
 import javafx.application.Application;
@@ -8,16 +13,39 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Java_todo_project {
-
+/**
+ *
+ * @author Maciej
+ */
+public class Java_todo_project extends Application {
+    
+    @Override
     public void start(Stage primaryStage) {
- 
+        Button btn = new Button();
+        btn.setText("+");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("+");
+            }
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        
+        Scene scene = new Scene(root, 300, 250);
+        
         primaryStage.setTitle("ToDo List");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
